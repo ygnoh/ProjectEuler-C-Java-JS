a251bec5e194a27252e521f51fa40e2b867d3480
yonggoonoh <nohyg90@gmail.com>
2016-05-09 00:12:45 +0900

    Coding convention

--- problem001/problem001.c
+++ problem001/problem001.c
@@ -6,10 +6,9 @@
 #include <stdio.h>
 
 int main(){
-	int i;
-	int sum = 0;
-	for(i = 1; i < 1000; i++)
-		if(i%3 == 0 || i%5 == 0)
+	int i, sum;
+	for (sum = 0, i = 1; i < 1000; i++)
+		if (i%3 == 0 || i%5 == 0)
 			sum += i;
 	printf("%d\n", sum);
 }
